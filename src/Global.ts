2889dfe2e714f421feb69d96785eedf8c7a238d6
Karl Oskar Anderson <72770213+oskar-anderson@users.noreply.github.com>
2023-09-07 23:25:01 +0300

    trying Github Pages reverting to 00aca47

--- src/Global.ts
+++ src/Global.ts
@@ -1,1 +1,2 @@
-export const ROOT_URL = `${window.location.protocol}//${window.location.host}`;
+export const HOST_SUFFIX = window.location.hostname === "localhost" ? "" : "/HedgehogDBD-hosting-test";
+export const ROOT_URL = `${window.location.protocol}//${window.location.host}${HOST_SUFFIX}`;
